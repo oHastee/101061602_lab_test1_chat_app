e42641fedbc2d2d5e249e711c03e02706c82b64f
oHastee <oscarpiediaz@gmail.com>
2025-02-05 10:20:57 -0500

    User.js model

---
+++ models/User.js
@@ -0,0 +1,8 @@
+const userSchema = new mongoose.Schema({
+    username: { type: String, required: true, unique: true },
+    firstname: { type: String, required: true },
+    lastname: { type: String, required: true },
+    password: { type: String, required: true },
+    createdOn: { type: Date, default: Date.now }
+});
+module.exports = mongoose.model('User', userSchema);
